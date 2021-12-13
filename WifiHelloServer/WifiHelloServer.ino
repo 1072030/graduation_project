d79a84d8883ec439652d398554ee89c83c2dadf4
1072030 <229jdu@gmail.com>
2021-12-13 22:41:22 +0800

    update wifi

--- WifiHelloServer/WifiHelloServer.ino
+++ WifiHelloServer/WifiHelloServer.ino
@@ -1,93 +1,119 @@
-/*
-  Make sure your Firebase project's ".read" and ".write" rules are set to "true".
-  Avoiding this will result in an error.
-  For more details- https://github.com/Rupakpoddar/ESP8266Firebase/blob/master/README.md
-*/
+#include <ArduinoJson.h>
 
-#include <ESP8266Firebase.h>
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
 #include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
 
-#define _SSID "AmyChen"        // Your WiFi SSID
-#define _PASSWORD "0982355465"    // Your WiFi Password
-#define PROJECT_ID "beacon-backend"
-   // Your Firebase Project ID. Can be found in project settings.
-
-/*
-  // FAST METHOD
-  // REQUIRES PERIODIC UPDATE OF FINGERPRINT (EVERY 1 YEAR)
-  // OUTDATED FINGERPRINT WILL STOP THE SERVICE
-  // LATEST FINGERPRINT CAN BE FOUND AT- https://www.grc.com/fingerprints.htm?domain=test.firebaseio.com
-  // REPLACE COLONS(:) WITH A SPACE(" ") AS SHOWN BELOW
-  // 03:9E:4F:E6:83:FC:40:EF:FC:B2:C5:EF:36:0E:7C:3C:42:20:1B:8F ==> 03 9E 4F E6 83 FC 40 EF FC B2 C5 EF 36 0E 7C 3C 42 20 1B 8F
-  #define FINGERPRINT "03 9E 4F E6 83 FC 40 EF FC B2 C5 EF 36 0E 7C 3C 42 20 1B 8F"
-  Firebase firebase(PROJECT_ID, FINGERPRINT);
-  // UNCOMMENT THIS BLOCK AND COMMENT THE LINE BELOW
-*/
-
-Firebase firebase(PROJECT_ID);    // SLOW BUT HASTLE-FREE METHOD FOR LONG TERM USAGE. DOES NOT REQUIRE PERIODIC UPDATE OF FINGERPRINT
-
+// Set these to run example.
+#define FIREBASE_HOST "beacon-backend-a8bf3-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_AUTH "s5deCRVxfQdCAOVkjJ4T1MPBE2nsOEmFPcEyjeRk"
+#define WIFI_SSID "AmyChen"
+#define WIFI_PASSWORD "0982355465"
+#define LedPin 16  
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(1000);
-
-  // Connect to WiFi
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to: ");
-  Serial.println(_SSID);
-  WiFi.begin(_SSID, _PASSWORD);
-
+  pinMode(LedPin, OUTPUT);
+  Serial.begin(9600);
+  
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
-    Serial.print("-");
   }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-  Serial.println("");
-  Serial.println("WiFi Connected");
-
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-  digitalWrite(LED_BUILTIN, HIGH);
-
-//================================================================//
-//================================================================//
-
-  // Examples of setting String, integer and float values.
-  firebase.setString("Example/setString", "It's Working");
-  firebase.setInt("Example/setInt", 123);
-  firebase.setFloat("Example/setFloat", 45.32);
-
-  // Examples of pushing String, integer and float values.
-  firebase.pushString("push", "Hello");
-  firebase.pushInt("push", 789);
-  firebase.pushFloat("push", 89.54);
-
-  // Example of getting a String.
-  String data1 = firebase.getString("Example/setString");
-  Serial.print("Received String: ");
-  Serial.println(data1);
-
-  // Example of getting an int.
-  int data2 = firebase.getInt("Example/setInt");
-  Serial.print("Received int: ");
-  Serial.println(data2);
-
-  // Example of getting a float.
-  float data3 = firebase.getFloat("Example/setFloat");
-  Serial.print("Received float: ");
-  Serial.println(data3);
-
-  // Example of data deletion.
-  firebase.deleteData("Example");
 }
-
+String status;
 void loop() {
-  // Nothing
+  status = Firebase.getString("/switch/light");
+  
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }else{
+    if(status.toInt() == 1){
+      Serial.println(status);
+      digitalWrite(LedPin, LOW);
+    }else{
+      Serial.println(status);
+      digitalWrite(LedPin, HIGH);
+    }
+  }
+  
+  delay(500);
+//  
+//  // update value
+//  Firebase.setFloat("number", 43.0);
+//  // handle error
+//  if (Firebase.failed()) {
+//      Serial.print("setting /number failed:");
+//      Serial.println(Firebase.error());  
+//      return;
+//  }
+//  delay(1000);
+//
+//  // get value 
+//  Serial.print("number: ");
+//  Serial.println(Firebase.getFloat("number"));
+//  delay(1000);
+//
+//  // remove value
+//  Firebase.remove("number");
+//  delay(1000);
+//
+//  // set string value
+//  Firebase.setString("message", "hello world");
+//  // handle error
+//  if (Firebase.failed()) {
+//      Serial.print("setting /message failed:");
+//      Serial.println(Firebase.error());  
+//      return;
+//  }
+//  delay(1000);
+//  
+//  // set bool value
+//  Firebase.setBool("truth", false);
+//  // handle error
+//  if (Firebase.failed()) {
+//      Serial.print("setting /truth failed:");
+//      Serial.println(Firebase.error());  
+//      return;
+//  }
+//  delay(1000);
+//
+//  // append a new value to /logs
+//  // handle error
+//  if (Firebase.failed()) {
+//      Serial.print("pushing /logs failed:");
+//      Serial.println(Firebase.error());  
+//      return;
+//  }
+//  Serial.print("pushed: /logs/");
+//
+//  delay(1000);
 }
