86e1a59ad5f9c9ce856fb8ad2c5aeb383571dae9
1072030 <229jdu@gmail.com>
2022-01-24 16:43:23 +0800

    #update rfid repeat

--- RFID-lock/RFID-lock.ino
+++ RFID-lock/RFID-lock.ino
@@ -8,7 +8,7 @@
 
 
 char *reference;
-#define relayPin  2                    // 連接繼電器腳位                       
+int relayPin=2;                    // 連接繼電器腳位                       
 byte uid[]={0xB3, 0x8C, 0x4A, 0x16};  //這是我們指定的卡片UID，可由讀取UID的程式取得特定卡片的UID，再修改這行
 //byte carduid[] = {0x53,0xDA,0xB9,0x16};
 MFRC522 mfrc522;   // 建立 MFRC522
@@ -16,59 +16,71 @@ MFRC522 mfrc522;   // 建立 MFRC522
 void setup()
 {
             // 設定繼電器接腳為輸出腳位      // 設定綠LED燈接腳為輸出腳位
-  
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
   SPI.begin();
   mfrc522.PCD_Init(SS_PIN, RST_PIN);
-  
   digitalWrite(relayPin, HIGH);
-  
   Serial.println("可開始讀取卡片");
   Serial.println();
- 
 }
 
 void loop() {
-  //Serial.print("reading...");
-  // 檢查是不是偵測到新的卡
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-      // 顯示卡片的UID
-      Serial.print(F("Card UID:"));
-      dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID
-      Serial.println();
-      Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-      Serial.println(mfrc522.PICC_GetTypeName(piccType));  //顯示卡片的類型
-      
-      //把取得的UID，拿來比對我們指定好的UID
-      bool they_match = true; // 初始值是假設為真 
-      for ( int i = 0; i < 4; i++ ) { // 卡片UID為4段，分別做比對
-        if ( uid[i] != mfrc522.uid.uidByte[i] ) { 
-          they_match = false; // 如果任何一個比對不正確，they_match就為false，然後就結束比對
-          break;
-        }
-      }
-      
-      //在監控視窗中顯示比對的結果
-      if(they_match){
-        Serial.println(F("Match Success!"));
-        digitalWrite(relayPin, LOW);
-        delay(1000);
-      }else{
-        Serial.println(F("Match Failed!"));
-        delay(500);
-      }
-      digitalWrite(relayPin, HIGH);
-    }
+ // 是否為新卡？
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  
+  // 選擇一張卡
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  
+  // 在監控視窗顯示UID
+  Serial.print("Card No. :");
+  
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  // 讓在讀卡區的RFID卡進入休眠狀態，不再重複讀卡
+//  mfrc522.PICC_HaltA();
+//  // 停止讀卡模組編碼  
+//  mfrc522.PCD_StopCrypto1();
+  Serial.println();
+  Serial.print(" Message : ");
+  content.toUpperCase();
+  // 需改成自己RFID卡的UID
+  if (content.substring(1) == "B3 8C 4A 16")
+  {
+    Serial.println(content.substring(1));
+    Serial.println(" 合法卡... ");
+    Serial.println();
+    digitalWrite(relayPin, LOW);               // 繼電器常開端(NO)導通
+    delay(2000);                                // 延遲2秒
+    digitalWrite(relayPin, HIGH);                // 繼電器常閉端(NC)導通
+    mfrc522.PCD_Init(SS_PIN, RST_PIN);
+  }
+  else
+  {
+    Serial.println(" 不合法卡...");
+    Serial.println();
+    digitalWrite(relayPin, HIGH);                // 繼電器常閉端(NC)導通                            
+  }
 }
-
 /**
  * 這個副程式把讀取到的UID，用16進位顯示出來
  */
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
+//void dump_byte_array(byte *buffer, byte bufferSize) {
+//  for (byte i = 0; i < bufferSize; i++) {
+//    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+//    Serial.print(buffer[i], HEX);
+//  }
+//}
