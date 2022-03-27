4a4a4c937fcdfe7f3ef9ac94ac51b5e4d208428a
1072030 <229jdu@gmail.com>
2022-03-27 14:18:58 +0800

    update ALL change

--- bluetooth_module/bluetooth_slave/bluetooth_slave.ino
+++ bluetooth_module/bluetooth_slave/bluetooth_slave.ino
@@ -1,28 +1,119 @@
-#include  <SoftwareSerial.h>
-// 定義連接藍牙模組的序列埠
-SoftwareSerial BT(10, 11); // 接收腳, 傳送腳
-char val;  // 儲存接收資料的變數
-const byte LED_PIN = 13;  // 定義LED腳位
+#include  <SoftwareSerial.h>              //  引用<SoftwareSerial.h>函式庫
 
-void setup() {
-  // 設定HC-05藍牙模組，AT命令模式的連線速率。
-  BT.begin(38400);
-  Serial.println("Ready to catch");
-  pinMode(LED_PIN, OUTPUT);
-}
+//***定義變數區***
 
-void loop() {
-  // 若收到藍牙模組的資料，則送到「序列埠監控視窗」
-  if (BT.available()) {
-    val = BT.read();
-    if (val == '1') {
-      digitalWrite(LED_PIN, HIGH);
-      // 回應命令發送端，告知「已開燈」
-      BT.println("LED ON");
-    } else if (val == '0') {
-      digitalWrite(LED_PIN, LOW);
-      // 回應命令發送端，告知「已關燈」
-      BT.println("LED OFF");
-    }
-  }
-}
+#define Bluetooth_KEY 13
+
+//  定義藍芽傳輸模組KEY接腳連接至arduino第9接腳
+
+#define Bluetooth_RxD 9
+
+//  定義藍芽傳輸模組RxD接腳連接至arduino第11接腳
+
+#define Bluetooth_TxD 8
+
+//  定義藍芽傳輸模組TxD接腳連接至arduino第10接腳
+
+SoftwareSerial BTSerial(Bluetooth_TxD, Bluetooth_RxD);
+
+//  建立軟體定義串列埠BTSerial，用以控制藍芽模組
+
+void setup()                             //  setup程式
+
+{                                      //  進入setup程式
+
+  pinMode(Bluetooth_KEY, OUTPUT);
+
+  //  設定arduino連接藍芽傳輸模組KEY之接腳為輸出
+
+  digitalWrite(Bluetooth_KEY, LOW);
+
+  //  設定藍芽傳輸模組KEY接腳為HIGH(進入AT command模式)
+
+  Serial.begin(38400);
+
+  //  開啟 Serial Port透過USB(uart)方式與電腦通信，鮑率為 38400bps (Bits Per Second)
+
+  Serial.print("Enter AT commands:");
+
+  //  透過USB(uart)傳輸字串"Enter AT commands:"
+
+  BTSerial.begin(38400);
+
+  //  設定控制HC-05藍芽模組之串列埠BTSerial鮑率為38400bps，此亦為HC-05藍芽模組預設鮑率
+
+}                                      //  結束setup程式
+
+void loop()                              //  loop程式
+
+{                                      //  進入loop程式
+
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+
+  if (BTSerial.available())
+
+    //  若連接至藍芽模組之Serialport接收到字元
+
+  {                                    //  進入if敘述
+
+    char BTSerial_read;
+
+    //  宣告BTSerial_read字元變數，用於記錄BTSerial.read()回傳字元
+
+    BTSerial_read = BTSerial.read();
+
+    //  將BTSerial.read()回傳字元填入BTSerial_read
+
+    Serial.write(BTSerial_read);
+
+    //  將BTSerial_read回傳至電腦
+
+    if(BTSerial_read == '\n')
+
+    //  若BTSerial_read為換行字元
+
+    {                                  //  進入if敘述
+
+      delay(10);                         //  延遲10ms
+
+      if(BTSerial.available() == 0)          //  若資料傳輸結束
+
+      {                                //  進入if敘述
+
+        Serial.print("Enter AT commands:");
+
+        //  透過USB(uart)傳輸字串"Enter AT commands:"
+
+      }                                //  結束if敘述
+
+    }                                  //  結束if敘述
+
+  }                                    //  結束if敘述
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+
+  if (Serial.available())
+
+  //  若與電腦連線之Serial Port接收到字元
+
+  {                                    //  進入if敘述
+
+    char Serial_read;
+
+    //  宣告Serial_read字元變數，用於記錄Serial.read()回傳字元
+
+    Serial_read = Serial.read();
+    
+    //  將Serial.read()回傳字元填入Serial_read
+
+    Serial.print(Serial_read);
+
+    //  將Serial_read字元回傳至電腦
+
+    BTSerial.write(Serial_read);
+
+    //  將Serial_read字元傳送至藍芽模組
+
+  }                                    //  結束if敘述
+
+}                                      //  結束loop程式
---
+++ rfid-cabinet/rfid-cabinet.ino
@@ -0,0 +1,168 @@
+
+/**
+   --------------------------------------------------------------------------------------------------------------------
+   Example sketch/program showing how to read data from more than one PICC to serial.
+   --------------------------------------------------------------------------------------------------------------------
+   This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+
+   Example sketch/program showing how to read data from more than one PICC (that is: a RFID Tag or Card) using a
+   MFRC522 based RFID Reader on the Arduino SPI interface.
+
+   Warning: This may not work! Multiple devices at one SPI are difficult and cause many trouble!! Engineering skill
+            and knowledge are required!
+
+   @license Released into the public domain.
+
+   Typical pin layout used:
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS 1    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required *
+   SPI SS 2    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required *
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+
+*/
+
+#include <SPI.h>
+#include <MFRC522.h>
+// PIN Numbers : RESET + SDAs
+#define RST_PIN         9
+#define SS_1_PIN        7
+#define SS_2_PIN        6
+
+// Led and Relay PINS
+#define relayIN         3
+
+
+// List of Tags UIDs that are allowed to open the puzzle
+byte tagarray[][4] = {
+  {0xB3, 0x8C, 0x4A, 0x16},
+  {0x53, 0xDA, 0xB9, 0x16}, 
+};
+
+// Inlocking status :
+int tagcount = 0;
+int reader1_count = 0;
+int reader2_count = 0;
+String content = "";
+String tagContent = "";
+#define NR_OF_READERS   2
+byte tagArray[][4]={
+  {0xFA , 0xC6 , 0x87 ,0x26},
+  {0xFA , 0xED , 0x06 ,0x26}
+};
+byte ssPins[] = {SS_1_PIN, SS_2_PIN};
+
+// Create an MFRC522 instance :
+MFRC522 mfrc522[NR_OF_READERS];
+
+/**
+   Initialize.
+*/
+void setup() {
+
+  Serial.begin(9600);           // Initialize serial communications with the PC
+  while (!Serial);              // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+
+  SPI.begin();                  // Init SPI bus
+
+  /* Initializing Inputs and Outputs */
+  pinMode(relayIN, OUTPUT);
+  digitalWrite(relayIN, HIGH);
+  /* looking for MFRC522 readers */
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+    mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN);
+    Serial.print(F("Reader "));
+    Serial.print(reader);
+    Serial.print(F(": "));
+    mfrc522[reader].PCD_DumpVersionToSerial();
+    //mfrc522[reader].PCD_SetAntennaGain(mfrc522[reader].RxGain_max);
+  }
+}
+
+/*
+   Main loop.
+*/
+
+void loop() {
+
+  CheckAgain(0);
+  CheckAgain(1);
+    if(reader1_count == 1){
+      Serial.println("Loop");
+      CheckAgain(1);
+    }else if(reader2_count == 1){
+      Serial.println("Loop");
+      CheckAgain(0);
+    }
+    delay(200);
+   if(reader1_count && reader2_count){
+     InitApp();
+   }
+   ResetVariable();
+}
+
+/**
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+void InitApp(){
+  digitalWrite(relayIN,LOW);
+  reader1_count = 0;
+  reader2_count = 0;
+  Serial.println("InitApp");
+  
+  delay(5000); //5秒緩衝reset
+  digitalWrite(relayIN, HIGH);
+   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+    mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN);
+    Serial.print(F("Reader "));
+    Serial.print(reader);
+    Serial.print(F(": "));
+    mfrc522[reader].PCD_DumpVersionToSerial();
+  }
+}
+void ResetVariable(){
+  reader1_count = 0;
+  reader2_count = 0;
+}
+void CheckAgain(int readerId){
+  if (mfrc522[readerId].PICC_IsNewCardPresent() && mfrc522[readerId].PICC_ReadCardSerial()) {
+      content = "";
+      tagContent = "";
+      Serial.print(F("Reader "));
+      Serial.print(readerId);
+      Serial.print(F(": Card UID:"));
+      dump_byte_array(mfrc522[readerId].uid.uidByte, mfrc522[readerId].uid.size);
+      for (byte i = 0; i < mfrc522[readerId].uid.size; i++)
+      {
+        Serial.println(mfrc522[readerId].uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.println(mfrc522[readerId].uid.uidByte[i], HEX);
+        content.concat(String(mfrc522[readerId].uid.uidByte[i] < 0x10 ? " 0" : " "));
+        content.concat(String(mfrc522[readerId].uid.uidByte[i], HEX));
+          tagContent.concat(String(tagArray[readerId][i] < 0x10 ? " 0" : " "));
+          tagContent.concat(String(tagArray[readerId][i], HEX));
+      }
+      content.toUpperCase();
+      tagContent.toUpperCase();
+        if(content.substring(1) == tagContent.substring(1) && readerId == 0){
+          Serial.println("reader : 0 Match");
+          reader1_count = reader1_count+1;
+        }
+        if(content.substring(1) == tagContent.substring(1) && readerId == 1){
+          Serial.println("reader : 1 Match");
+          reader2_count = reader2_count+1;
+        }  
+      }
+}
